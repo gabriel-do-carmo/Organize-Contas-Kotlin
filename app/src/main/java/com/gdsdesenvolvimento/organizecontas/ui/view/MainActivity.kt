a9d6e52b5cb4ea84a1c9d73f828158beac691725
Gabriel Do Carmo <83257171+G7Carmo@users.noreply.github.com>
2022-06-19 19:24:34 -0300

    init

---
+++ app/src/main/java/com/gdsdesenvolvimento/organizecontas/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gdsdesenvolvimento.organizecontas.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdsdesenvolvimento.organizecontas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
